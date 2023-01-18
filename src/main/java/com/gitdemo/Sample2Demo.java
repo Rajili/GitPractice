3ac63fc8389089ba93cba974bd90a85e5e8bce31
rajili <rakhimay29@gmail.com>
2023-01-18 16:08:29 +0530

    First commit...;

---
+++ src/main/java/com/gitdemo/Sample2Demo.java
@@ -0,0 +1,11 @@
+package com.gitdemo;
+
+public class Sample2Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloooo...");
+
+	}
+
+}
