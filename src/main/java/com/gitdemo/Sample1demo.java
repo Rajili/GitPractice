7e2cf9bc3c1e2ea02804b56bfcbeb8326621d48a
rajili <rakhimay29@gmail.com>
2023-01-18 16:28:43 +0530

    commit from new WS;

--- src/main/java/com/gitdemo/Sample1demo.java
+++ src/main/java/com/gitdemo/Sample1demo.java
@@ -5,6 +5,7 @@ public class Sample1demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello");
+System.out.println("This is new line added");
 	}
 
 }
